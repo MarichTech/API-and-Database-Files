822fab2763614a7baea3c5020c156857d9b88c81
Cyrus-Muchiri <cyrusmuchirichomba@gmail.com>
2021-04-09 00:28:52 +0300

    Assigning Orders to agents

--- application/models/Reports_model.php
+++ application/models/Reports_model.php
@@ -73,7 +73,7 @@ class Reports_model extends CI_Model
 		foreach ($params as $key => $value) {
 			if ($value != null) {
 				$this->db->where("$key", $value);
-			}else{
+			}else if(!strpos($key,"=")){
 				$this->db->where("$key");
 			}
 		}
